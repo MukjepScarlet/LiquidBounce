995ba180426d8cd19ed83124b192d31f26521052
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2025-07-13 00:40:51 +0800

    forgot this...

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -31,6 +31,7 @@ import net.ccbluex.liquidbounce.features.module.modules.client.ModuleRichPresenc
 import net.ccbluex.liquidbounce.features.module.modules.client.ModuleTargets
 import net.ccbluex.liquidbounce.features.module.modules.combat.*
 import net.ccbluex.liquidbounce.features.module.modules.combat.aimbot.ModuleAutoBow
+import net.ccbluex.liquidbounce.features.module.modules.combat.aimbot.ModuleDroneControl
 import net.ccbluex.liquidbounce.features.module.modules.combat.autoarmor.ModuleAutoArmor
 import net.ccbluex.liquidbounce.features.module.modules.combat.backtrack.ModuleBacktrack
 import net.ccbluex.liquidbounce.features.module.modules.combat.criticals.ModuleCriticals
@@ -90,6 +91,7 @@ import net.ccbluex.liquidbounce.script.ScriptApiRequired
 import net.ccbluex.liquidbounce.utils.client.logger
 import net.ccbluex.liquidbounce.utils.client.mc
 import net.ccbluex.liquidbounce.utils.input.InputBind
+import net.ccbluex.liquidbounce.utils.input.toModifierOrNull
 import net.ccbluex.liquidbounce.utils.kotlin.mapArray
 import net.ccbluex.liquidbounce.utils.kotlin.sortedInsert
 import org.lwjgl.glfw.GLFW
@@ -115,15 +117,19 @@ object ModuleManager : EventListener, Iterable<ClientModule> by modules {
     private val keyboardKeyHandler = handler<KeyboardKeyEvent> { event ->
         when (event.action) {
             GLFW.GLFW_PRESS -> if (mc.currentScreen == null) {
-                    filter { m -> m.bind.matchesKey(event.keyCode, event.scanCode) }
-                    .forEach { m ->
-                        m.enabled = !m.enabled || m.bind.action == InputBind.BindAction.HOLD
-                    }
+                filter { m ->
+                    m.bind.matchesKey(event.keyCode, event.scanCode) && m.bind.matchesModifiers(event.mods)
+                }.forEach { m ->
+                    m.enabled = !m.enabled || m.bind.action == InputBind.BindAction.HOLD
                 }
+            }
+
             GLFW.GLFW_RELEASE ->
                 filter { m ->
-                    m.bind.matchesKey(event.keyCode, event.scanCode) &&
-                        m.bind.action == InputBind.BindAction.HOLD
+                    m.bind.action == InputBind.BindAction.HOLD && (
+                        m.bind.matchesKey(event.keyCode, event.scanCode)
+                            || event.key.toModifierOrNull().let { it in m.bind.modifiers && !it!!.isAnyPressed }
+                        )
                 }.forEach { m ->
                     m.enabled = false
                 }
@@ -134,14 +140,18 @@ object ModuleManager : EventListener, Iterable<ClientModule> by modules {
     private val mouseButtonHandler = handler<MouseButtonEvent> { event ->
         when (event.action) {
             GLFW.GLFW_PRESS -> if (mc.currentScreen == null) {
-                filter { m -> m.bind.matchesMouse(event.button) }
+                filter { m -> m.bind.matchesMouse(event.button) && m.bind.matchesModifiers(event.mods) }
                     .forEach { m ->
                         m.enabled = !m.running || m.bind.action == InputBind.BindAction.HOLD
                     }
             }
+
             GLFW.GLFW_RELEASE ->
                 filter { m ->
-                    m.bind.matchesMouse(event.button) && m.bind.action == InputBind.BindAction.HOLD
+                    m.bind.action == InputBind.BindAction.HOLD && (
+                        m.bind.matchesMouse(event.button)
+                            || event.key.toModifierOrNull().let { it in m.bind.modifiers && !it!!.isAnyPressed }
+                        )
                 }.forEach { m -> m.enabled = false }
         }
     }
