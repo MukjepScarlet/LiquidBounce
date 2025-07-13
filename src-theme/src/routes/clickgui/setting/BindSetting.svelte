a5b2dabac9e7d4a633e3c70feb8b26e832b39d0d
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-07-13 20:30:14 +0800

    succeeded

--- src-theme/src/routes/clickgui/setting/BindSetting.svelte
+++ src-theme/src/routes/clickgui/setting/BindSetting.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-    import {createEventDispatcher} from "svelte";
+    import {createEventDispatcher, onDestroy} from "svelte";
     import type {BindModifier, BindSetting, ModuleSetting, Screen} from "../../../integration/types";
-    import {listen, waitNext} from "../../../integration/ws";
+    import {listen, waitMatches} from "../../../integration/ws";
     import {getPrintableKeyName} from "../../../integration/rest";
     import type {KeyboardKeyEvent, MouseButtonEvent} from "../../../integration/events";
     import {convertToSpacedString, spaceSeperatedNames} from "../../../theme/theme_config";
     import Dropdown from "./common/Dropdown.svelte";
-    import MultiChoiceItem from "./common/MultiChoiceItem.svelte";
 
     export let setting: ModuleSetting;
 
@@ -32,6 +31,56 @@
     const isClickGuiScreen = (screen: Screen | undefined) =>
         !(screen === undefined || !screen.class.startsWith("net.ccbluex.liquidbounce") || screen.title !== "ClickGUI" && screen.title !== "VS-CLICKGUI")
 
+    /**
+     * Gets the next possible event which can be used as a bind.
+     */
+    const nextBindEvent = () => Promise.any([
+        waitMatches("mouseButton", (e: MouseButtonEvent) =>
+            isClickGuiScreen(e.screen) && !(e.button === 0 /* LMB */ && isHovered)
+        ),
+        waitMatches("keyboardKey", (e: KeyboardKeyEvent) =>
+            isClickGuiScreen(e.screen)
+        ),
+    ]);
+
+    let addedModifiers = new Set<BindModifier>();
+
+    /**
+     * Tries to handle the event. If it's consumed
+     * @return undefined if it's not a modifier, or else its key name and parsed modifier
+     */
+    const handleBindEventIfNotModifier = (event: MouseButtonEvent | KeyboardKeyEvent) => {
+        if (Object.hasOwn(event, 'keyCode')) {
+            const e = event as KeyboardKeyEvent;
+            if (e.keyCode === 256 /* GLFW_KEY_ESCAPE */) {
+                handleActionChange(UNKNOWN_KEY);
+                return undefined;
+            }
+
+            const modifierOrUndef = KEY_CODE_TO_MODIFIERS[e.keyCode];
+
+            if (!modifierOrUndef) {
+                handleActionChange(e.key);
+                return undefined;
+            }
+
+            return { key: e.key, modifier: modifierOrUndef };
+        } else if (Object.hasOwn(event, 'button')) {
+            const e = event as MouseButtonEvent;
+            handleActionChange(e.key);
+            return undefined;
+        } else {
+            throw new Error("Unexcepted event: " + JSON.stringify(event));
+        }
+    }
+
+    let timeout: ReturnType<typeof setTimeout> | undefined = undefined;
+    onDestroy(() => {
+        if (timeout !== undefined) {
+            clearTimeout(timeout);
+        }
+    });
+
     async function toggleBinding() {
         // Binding progress -> cancel it
         if (binding) {
@@ -41,43 +90,57 @@
 
         binding = true;
 
-        const firstIncomingEvent = await Promise.any([
-            waitNext("mouseButton", (e: MouseButtonEvent) =>
-                isClickGuiScreen(e.screen) && !(e.button === 0 /* LMB */ && isHovered)
-            ),
-            waitNext("keyboardKey", (e: KeyboardKeyEvent) =>
-                isClickGuiScreen(e.screen)
-            ),
-        ]);
+        let event = await nextBindEvent();
 
         // Promise doesn't support cancellation, so we need manual check
         if (!binding) return;
 
-        if (Object.hasOwn(firstIncomingEvent, 'keyCode')) {
-            const e = firstIncomingEvent as KeyboardKeyEvent;
-            if (e.keyCode === 256 /* GLFW_KEY_ESCAPE */) {
-                handleActionChange(UNKNOWN_KEY);
-                return;
+        let result = handleBindEventIfNotModifier(event);
+        while (result) {
+            if (timeout !== undefined) {
+                clearTimeout(timeout);
             }
+            const { key, modifier } = result;
 
-            handleActionChange(e.key);
-            return;
-        } else if (Object.hasOwn(firstIncomingEvent, 'button')) {
-            const e = firstIncomingEvent as MouseButtonEvent;
-            handleActionChange(e.key);
-            return;
-        } else {
-            throw new Error("Unexcepted event: " + JSON.stringify(firstIncomingEvent));
+            addedModifiers.add(modifier);
+            addedModifiers = addedModifiers; // Trigger reactive update
+
+            timeout = setTimeout(() => {
+                if (binding) {
+                    handleActionChange(key);
+                }
+                timeout = undefined;
+            }, 1000);
+
+            event = await nextBindEvent();
+            if (!binding) return;
+            result = handleBindEventIfNotModifier(event);
         }
     }
 
     function handleActionChange(newBoundKey: string) {
         cSetting.value.boundKey = newBoundKey;
+        cSetting.value.modifiers = Array.from(addedModifiers);
+        addedModifiers.clear();
         binding = false;
         setting = {...cSetting};
         dispatch("change");
     }
 
+    /**
+     * Switch action among {@link BindAction}.
+     *
+     * TODO: rewrite this
+     */
+    function switchAction() {
+        setting = {...cSetting};
+        dispatch("change");
+    }
+
+    /**
+     * @deprecated
+     * @param modifier
+     */
     function toggleModifier(modifier: BindModifier) {
         if (cSetting.value.modifiers.includes(modifier)) {
             cSetting.value.modifiers = cSetting.value.modifiers.filter(it => it !== modifier);
@@ -86,11 +149,15 @@
         }
     }
 
-    const ALL_MODIFIERS = ["Shift", "Control", "Alt", "Super"] as const;
-
     /**
-     *
+     * https://www.glfw.org/docs/3.3/group__keys.html
      */
+    const KEY_CODE_TO_MODIFIERS: Record<number, BindModifier> = {
+        340: "Shift", 344: "Shift",
+        341: "Control", 345: "Control",
+        342: "Alt", 346: "Alt",
+        343: "Super", 347: "Super",
+    } as const;
 </script>
 
 <div class="setting" class:has-value={cSetting.value.boundKey !== UNKNOWN_KEY}>
@@ -108,24 +175,17 @@
             {:else}
                 <span>{printableKeyName}</span>
             {/if}
+        {:else if addedModifiers.size}
+            <span>{Array.from(addedModifiers).join(" + ")} + ...</span>
         {:else}
             <span>Press any key...</span>
         {/if}
     </button>
 
     {#if cSetting.value.boundKey !== UNKNOWN_KEY}
+        <!-- TODO: replace with click to switch... -->
         <Dropdown name={null} options={["Toggle", "Hold"]} bind:value={cSetting.value.action}
-                  on:change={handleActionChange} style="grid-area: action"/>
-        <div class="modifiers">
-            {#each ALL_MODIFIERS as modifier (modifier)}
-                <MultiChoiceItem
-                    active={cSetting.value.modifiers.includes(modifier)}
-                    error={false}
-                    onclick={() => toggleModifier(modifier)}
-                    content={$spaceSeperatedNames ? convertToSpacedString(modifier) : modifier}
-                />
-            {/each}
-        </div>
+                  on:change={switchAction} style="grid-area: action"/>
     {/if}
 </div>
 
@@ -141,9 +201,7 @@
     &.has-value {
       grid-template-columns: 1fr max-content;
       grid-template-rows: auto auto;
-      grid-template-areas:
-          "main action"
-          "modifiers modifiers";
+      grid-template-areas: "main action";
     }
 
     &:not(.has-value) {
@@ -175,15 +233,5 @@
     .none {
       color: $clickgui-text-dimmed-color;
     }
-  }
-
-  .modifiers {
-    display: flex;
-    flex-direction: row;
-    font-size: 12px;
-    gap: 7px;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    grid-area: modifiers;
   }
 </style>
